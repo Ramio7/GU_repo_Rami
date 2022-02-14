c28825f54a33bfb9a2eb55c47c2ec0517ce9ceeb
Ramio7 <Dunadan2007@yandex.ru>
2022-02-14 21:40:13 +0300

    added project for my homework

---
+++ 6 урок/Delegates, files, collections/Homework_6/Delegates, files, collections/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates__files__collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
