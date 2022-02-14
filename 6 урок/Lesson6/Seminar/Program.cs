cdb4a8aacbd1a629d5a5bb3e2b6ac8feedd663ee
Ramio7 <Dunadan2007@yandex.ru>
2022-02-14 21:32:52 +0300

    Added lecturers files from lesson

---
+++ 6 урок/Lesson6/Seminar/Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar
+{
+
+    class User
+    {
+        /*private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }*/
+
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public DateTime Birthday { get; set; }
+
+    }
+
+    class MyList<TElement>
+    {
+
+        private TElement element;
+
+        public TElement Element
+        {
+            get { return element; }
+        }
+
+        public MyList(TElement element)
+        {
+            this.element = element;
+        }
+
+
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ArrayList users = new ArrayList();
+
+            StreamReader reader = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "UserList.txt");
+            while (!reader.EndOfStream)
+            {
+                string[] words = reader.ReadLine().Split(' ');
+                User user = new User();
+                user.Name = words[1];
+                user.Surname = words[0];
+                user.Birthday = DateTime.Parse(words[2]);
+                users.Add(user);
+            }
+
+            users.Add(1);
+            users.Add(false);
+            users.Add("ABCD");
+
+            reader.Close();
+
+            for (int i = 0; i < users.Count; i++)
+            {
+                if (users[i] is User)
+                {
+                    Console.WriteLine($"{((User)users[i]).Surname} {((User)users[i]).Name} {((User)users[i]).Birthday.ToShortDateString()}");
+                }
+                
+            }
+
+            //foreach (User user in users)
+            //{
+
+            //    Console.WriteLine($"{user.Surname} {user.Name} {user.Birthday.ToShortDateString()}");
+            //}
+
+
+            MyList<String> myList = new MyList<String>("Hello!");
+            MyList<Int32> myList1 = new MyList<Int32>(12);
+            List<User> users02 = new List<User>();
+
+            /*int[] arr01 = new int[3];
+            List<int> lst = new List<int>();
+            lst.Add(-5);
+            lst.Add(5);
+            lst.Add(6);*/
+
+
+            reader = new StreamReader(AppDomain.CurrentDomain.BaseDirectory + "UserList.txt");
+            while (!reader.EndOfStream)
+            {
+                string[] words = reader.ReadLine().Split(' ');
+                User user = new User();
+                user.Name = words[1];
+                user.Surname = words[0];
+                user.Birthday = DateTime.Parse(words[2]);
+                users02.Add(user);
+            }
+
+            users.Add(1);
+            users.Add(false);
+            users.Add("ABCD");
+
+            reader.Close();
+
+            for (int i = 0; i < users02.Count; i++)
+            {
+                Console.WriteLine($"{users02[i].Surname} {users02[i].Name} {users02[i].Birthday.ToShortDateString()}");
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 6 урок/Lesson6/Seminar/Sample02.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Seminar
+{
+
+
+    public class MyCollection<T>{
+
+        private T[] elements;
+        private int length = 0;
+
+        public MyCollection() { 
+
+            elements = new T[1000];
+        }
+
+        public void Add(T item){
+            // elements[length++] = item;
+            elements[length] = item;
+            length = length + 1;
+        }
+
+        public int Count
+        {
+            get
+            {
+                return length;
+            }
+        }
+
+    }
+
+    internal class Sample02
+    {
+        static void Main(string[] args)
+        {
+            MyCollection<User> myCollection = new MyCollection<User>();
+            myCollection.Add(new User());  
+            myCollection.Add(new User());
+            myCollection.Add(new User());
+
+            Console.WriteLine(myCollection.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
