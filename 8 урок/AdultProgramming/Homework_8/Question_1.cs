2523499b1493067add9e8d28f6d9ddfbec4a54d1
Ramio7 <Dunadan2007@yandex.ru>
2022-02-24 08:45:38 +0300

    Закончил домашнее задание №8. Интерфейс переделывать не стал т.к. научился делать новые цветовые схемы в предыдущем уроке, а текущее оформление мне нравится

---
+++ 8 урок/AdultProgramming/Homework_8/Question_1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_8
+{
+    internal class Question_1
+    {
+        static void Main(string[] args)
+        {
+            DateTime dateTime = new DateTime();
+            PropertyInfo[] properties;
+            properties = dateTime.GetType().GetProperties();
+            foreach (PropertyInfo property in properties)
+                Console.WriteLine(property.Name);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 8 урок/Lesson8/Seminar/Sample02.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Seminar
+{
+    internal class Sample02
+    {
+
+        public static Person LoadPersonFromXml(string fileName)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Person));
+            FileStream fileStream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            Person person = (Person)xmlSerializer.Deserialize(fileStream);
+            fileStream.Close();
+            return person;
+        }
+
+        public static void SavePersonToXml(string fileName, Person person)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(Person));
+            FileStream fileStream = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            xmlSerializer.Serialize(fileStream, person);
+            fileStream.Close();
+        }
+
+        public static void Main()
+        {
+            Person person = LoadPersonFromXml(AppDomain.CurrentDomain.BaseDirectory + "Person.xml");
+            if (person != null)
+            {
+                Console.WriteLine($"{person.LastName} {person.FirstName} {person.SecondName} {person.Age}");
+
+                Console.ReadLine();
+            }
+
+            person.Age = 36;
+            person.FirstName = "Stanislav";
+
+            SavePersonToXml(AppDomain.CurrentDomain.BaseDirectory + "Person.new.xml", person);
+
+            Console.WriteLine("\n***\n");
+
+            person = LoadPersonFromXml(AppDomain.CurrentDomain.BaseDirectory + "Person.new.xml");
+            if (person != null)
+            {
+                Console.WriteLine($"{person.LastName} {person.FirstName} {person.SecondName} {person.Age}");
+
+                Console.ReadLine();
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
